958ced42d0b4311d3c5bfb7a2cf15cb574295b03
Esmae <esmae.woods@diamond.ac.uk>
2017-07-07 10:58:06 +0100

    add debugging echos

--- update-gh.sh
+++ update-gh.sh
@@ -9,9 +9,12 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git clone --quiet --branch=testingUpload https://${TOKEN}@github.com/Esmae/Matrices2.git  testingUpload > /dev/null
 
   cp -Rf $HOME/test8 testingUpload
+  echo -e "1"
 
   git add -f .
+  echo -e "2"
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to testingUpload"
+  echo -e "3"
   git push -fq https://${TOKEN}@github.com/Esmae/Matrices2.git testingUpload > /dev/null
 
   echo -e "Success? \n"
